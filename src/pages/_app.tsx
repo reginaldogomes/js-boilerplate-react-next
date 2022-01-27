bdefd663b3e088bd8729d921c81de159de95213b
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2022-01-27 13:19:22 -0300

    configuration eslint, prettier, editorconfig

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from 'next/app'
-import GlobalStyle from '../styles/globalStyles'
+
+import { ThemeProvider } from 'styled-components'
+import theme from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,12 @@ import type { NextPage } from 'next'
 import styled from 'styled-components'
 
 const Title = styled.h1`
-  color: red;
-  font-size: 50px;
+    color: red;
+    font-size: 50px;
 `
 
 const Home: NextPage = () => {
-  return (
-    <Title>Hello World</Title>
-  )
+    return <Title>Hello World</Title>
 }
 
 export default Home
